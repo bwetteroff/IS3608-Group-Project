9332da0a72c5be14baffabe7a93ba749b7f4a177
brnthngs <bpwetteroff@gmail.com>
2015-10-03 00:17:28 -0500

    upload

---
+++ GroupProject/src/Benjamin/DataCollection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Write a program that will find the product of a collection of data values. 
+//The program should ignore all negative values and terminate when a zero value is encountered. 
+
+package Benjamin;
+
+/**
+ *
+ * @author wetteb
+ */
+public class DataCollection
+{
+    
+}
