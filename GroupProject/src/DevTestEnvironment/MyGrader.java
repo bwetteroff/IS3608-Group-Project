7920b43abcf2c6b7666438f814e9fc01837f2ea0
brnthngs <bpwetteroff@gmail.com>
2015-10-02 22:47:21 -0500

    updates

--- GroupProject/src/DevTestEnvironment/MyGrader.java
+++ GroupProject/src/DevTestEnvironment/MyGrader.java
@@ -1,3 +1,7 @@
+package DevTestEnvironment;
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -71,4 +75,4 @@ public class MyGrader
 
 }
 
-}
+
